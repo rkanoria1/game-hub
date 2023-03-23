3e36df1f178ebd509f7e366982455b9ae4099ebd
Rahul <rahul.kanoria@housing.com>
2023-03-23 19:06:11 +0530

    Resized images

---
+++ src/services/image-url.ts
@@ -0,0 +1,10 @@
+
+const getCroppedImageUrl = (url: string) => {
+    const target = 'media/';
+    const index = url.indexOf(target) + target.length;
+    const newURL = url.slice(0, index-1) + '/crop/600/400/' + url.slice(index);
+
+    return newURL;
+}
+
+export default getCroppedImageUrl;
